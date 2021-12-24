9edf627bf777b69d57f35555d428fb3ca28a871c
Harry Crane <37228043+hwcrane@users.noreply.github.com>
2021-12-24 17:22:48 +0000

    Update index.js

--- web/index.js
+++ web/index.js
@@ -3,7 +3,7 @@ import init, * as wasm from "./wasm.js";
 const WIDTH = 64;
 const HEIGHT = 32;
 let SCALE = Math.floor(window.innerWidth / 80) - Math.floor(window.innerWidth / 800);
-const TICKS_PER_FRAME = 8;
+const TICKS_PER_FRAME = 10;
 let anim_frame = 0;
 
 const canvas = document.getElementById("canvas");
